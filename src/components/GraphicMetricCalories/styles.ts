991c537e65d66f45a8362d337c22b4b8f6344f46
Jose Murillo <jooseemurillo@gmail.com>
2022-01-24 20:10:46 -0300

    fix: decrease padding-left in graphic

--- src/components/GraphicMetricCalories/styles.ts
+++ src/components/GraphicMetricCalories/styles.ts
@@ -1,13 +1,15 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  background-color: ${({theme}) => theme.colors.card};
+  /* background-color: ${({theme}) => theme.colors.card}; */
+  background-color: #F5F5F5;
+
   width: 100%;
   height: 162px;
   border-radius: 6px;
   margin-top: 4px;
 
-  padding: 18px 45px 16px 24px;
+  padding: 18px 45px 16px 12px;
 
   flex-direction: row;
   justify-content: space-around;
