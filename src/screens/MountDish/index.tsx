ed5df3e655c870010989c0f7f7fef2283c3775a0
Jose Murillo <jooseemurillo@gmail.com>
2022-01-26 19:26:29 -0300

    chore: simple load for listing foods

--- src/screens/MountDish/index.tsx
+++ src/screens/MountDish/index.tsx
@@ -12,20 +12,25 @@ import {
 import { Input } from "../../components/Form/Input";
 import { CardFood } from "../../components/CardFood";
 // import { foods } from "../../commons/foods";
-import { FlatList } from "react-native";
+import { ActivityIndicator, FlatList, Text } from "react-native";
 import { Food } from "./types";
 import { api } from "../../services/api";
+import theme from "../../global/styles/theme";
 
 export function MountDish() {
 
   const [foods, setFoods] = useState<Food[]>([]);
+  const [loading, setLoading] = useState(false);
 
   async function loadFoods() {
     try {
+      setLoading(true);
       const response = await api.get('/foods');
       setFoods(response.data);     
     } catch {
       console.log(`⭕ Tratar o error com alguma mensagem para o usuário final`)
+    } finally {
+      setLoading(false)
     }
       
   }
@@ -48,7 +53,14 @@ export function MountDish() {
         </TitleJoinLine>
 
         <WrapperCardFood>
-          <FlatList
+          {loading ? (
+            <ActivityIndicator 
+              color={theme.colors.primary} 
+              size="large" 
+              style={{ flex: 1, justifyContent: "flex-start"}} 
+            />
+          ): (
+            <FlatList
             showsVerticalScrollIndicator={false}
             data={foods}
             keyExtractor={food => food.nameFood}
@@ -60,6 +72,7 @@ export function MountDish() {
               />
             )}
           />
+          )}
         </WrapperCardFood>
 
 
