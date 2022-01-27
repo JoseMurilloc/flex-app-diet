44b05ad411562b3e30ea3b83edd61d91cf36f4e6
Jose Murillo <jooseemurillo@gmail.com>
2022-01-27 08:53:04 -0300

    chore: apply focus of input

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { TextInputProps, View } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 
@@ -6,15 +6,28 @@ import { Container, WrapperInput } from './styles';
 import theme from '../../../global/styles/theme';
 
 export function Input ({...rest}: TextInputProps) {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleInputFocus = useCallback(() => setIsFocused(true), []);
+  const handleInputBlur = useCallback(() => setIsFocused(false), []);
+
+
   return (
-    <WrapperInput>
+    <WrapperInput 
+      isFocused={isFocused}
+      isErrored={false}
+    >
       <AntDesign 
         name="search1" 
         size={15} 
-        color={theme.colors.description}
+        color={isFocused ? theme.colors.primary : theme.colors.description}
         style={{marginRight: 5}} 
       />
-      <Container {...rest}/>
+      <Container 
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        {...rest}
+      />
     </WrapperInput>
   );
 }
--- src/components/Form/Input/styles.ts
+++ src/components/Form/Input/styles.ts
@@ -1,14 +1,28 @@
-import styled from 'styled-components/native';
+import { lighten } from 'polished';
+import styled, { css } from 'styled-components/native';
 
-export const WrapperInput = styled.View`
+type WrapperInputProps = {
+  isFocused: boolean;
+  isErrored: boolean;
+}
+
+export const WrapperInput = styled.View<WrapperInputProps>`
   width: 100%;
   height: 50px;
-  background-color: ${({theme}) => theme.colors.card};
   border-radius: 30px;
   padding: 7px 0px 7px 20px;
-
+  background-color: ${({theme}) => theme.colors.card};
+  
   flex-direction: row;
   align-items: center;
+  
+  ${props => props.isFocused ? css`
+    border-color: ${({theme}) => theme.colors.primary};
+    border-width: 0.5px;
+  ` : css`
+    border-color: ${({theme}) => theme.colors.card};
+    border-width: 0.5px;
+  `}
 `;
 
 export const Container = styled.TextInput.attrs(props => ({}))`
