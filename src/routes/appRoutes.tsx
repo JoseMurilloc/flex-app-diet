dbe94d7d80ca41d8b545e0c6d39a8a8269089a3e
Jose Murillo <jooseemurillo@gmail.com>
2022-01-27 21:38:37 -0300

    chore: add screen in routes of MyDish

--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { Home } from '../screens/Home';
 import { MountDish } from '../screens/MountDish';
 import theme from '../global/styles/theme';
-// import { Ionicons } from '@expo/vector-icons';
+import { MyDish } from '../screens/MyDish';
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  Home: undefined;
+  MountDish: undefined;
+  MyDish: undefined;
+};
+
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 
 export function AppRoutes() {
@@ -28,6 +34,22 @@ export function AppRoutes() {
         component={MountDish}
         options={{ 
           title: "Montar prato",
+          headerTintColor: '#FFF',
+          headerTitleStyle: { 
+            fontFamily: theme.fonts.bold,
+            fontSize: theme.sizes.large,
+            fontWeight: '700'
+          },
+          headerStyle: {
+            backgroundColor: theme.colors.primary,
+          },
+        }}
+      />
+      <Stack.Screen 
+        name="MyDish" 
+        component={MyDish}
+        options={{ 
+          title: "Meu prato",
           headerTintColor: '#FFF',
           headerTitleStyle: { 
             fontFamily: theme.fonts.bold,
