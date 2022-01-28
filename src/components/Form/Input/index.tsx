b95998f5f504cdb25dbe0802110ab6e19db5bd48
Jose Murillo <jooseemurillo@gmail.com>
2022-01-28 13:20:28 -0300

    chore: input controller for react-hook-form

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -5,9 +5,15 @@ import { AntDesign } from '@expo/vector-icons';
 import { Container, WrapperInput } from './styles';
 import theme from '../../../global/styles/theme';
 
-export function Input ({...rest}: TextInputProps) {
-  const [isFocused, setIsFocused] = useState(false);
+import { Controller, Control } from "react-hook-form";
 
+interface InputProps extends TextInputProps {
+  control: Control | any;
+  name: string;
+}
+
+export function Input ({ name, control,...rest}: InputProps) {
+  const [isFocused, setIsFocused] = useState(false);
   const handleInputFocus = useCallback(() => setIsFocused(true), []);
   const handleInputBlur = useCallback(() => setIsFocused(false), []);
 
@@ -23,10 +29,18 @@ export function Input ({...rest}: TextInputProps) {
         color={isFocused ? theme.colors.primary : theme.colors.description}
         style={{marginRight: 5}} 
       />
-      <Container 
-        onFocus={handleInputFocus}
-        onBlur={handleInputBlur}
-        {...rest}
+      <Controller
+        control={control}
+        name={name}
+        render={({ field: { onChange, value}}) => (
+          <Container 
+            onChangeText={onChange}
+            value={value}
+            onFocus={handleInputFocus}
+            onBlur={handleInputBlur}
+            {...rest}
+          />
+        )}
       />
     </WrapperInput>
   );
