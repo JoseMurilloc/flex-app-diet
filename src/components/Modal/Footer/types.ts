9745c3d4fada93d8a5ba1e790908d314243c33e2
Jose Murillo <jooseemurillo@gmail.com>
2022-02-03 16:54:41 -0300

    chore: Updated types of footer

--- src/components/Modal/Footer/types.ts
+++ src/components/Modal/Footer/types.ts
@@ -1,9 +1,8 @@
 import { UseFormHandleSubmit } from 'react-hook-form';
-import { Food } from '../../../contexts/meals/types';
-import { FormData } from '../../InfoFoodModal/types';
+import { FormRegisterData } from '../../RegisterFoodModal/type';
 
 export type FooterProps = {
   handleSubmit:  UseFormHandleSubmit<any>;
-  handleAddFoodInMeal?: (food: Food) => Promise<void>;
-  handleConfirmMeal?:  (data: FormData) => Promise<void>
+  handleAddFoodInMeal?: (food: FormRegisterData) => Promise<void>;
+  handleConfirmMeal?:  (data: any) => Promise<void>
 }
