bd207dcd913e97fed37907c331d9078f004ea156
José Murillo <jooseemurillo@gmail.com>
2022-02-22 09:25:05 -0300

    style: changed radius of button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components/native";
 export const Container = styled.TouchableOpacity`
   background-color: ${({theme}) => theme.colors.primary};
   height: 35px;
-  border-radius: 40px;
+  border-radius: 15px;
   justify-content: center;
   align-items: center;
   padding: 8px 23px;
