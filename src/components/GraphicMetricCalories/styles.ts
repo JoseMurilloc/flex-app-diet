32630d8d76b9a160b68c2b5b7821ee72b02b6d2c
José Murillo <jooseemurillo@gmail.com>
2022-02-26 01:27:03 -0300

    style: added color border in theme of  Graphic

--- src/components/GraphicMetricCalories/styles.ts
+++ src/components/GraphicMetricCalories/styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  /* background-color: ${({theme}) => theme.colors.card}; */
   background-color: #F5F5F5;
+  border: 0.5px solid ${({theme}) => theme.colors.border};
 
   width: 100%;
   height: 162px;
