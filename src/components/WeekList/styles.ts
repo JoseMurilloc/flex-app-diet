46874742b0885179ae4ed4d1e78164ea556cf9ad
José Murillo <jooseemurillo@gmail.com>
2022-02-26 01:27:33 -0300

    style: rebuild UI of week list

--- src/components/WeekList/styles.ts
+++ src/components/WeekList/styles.ts
@@ -6,10 +6,11 @@ export const Container = styled.View`
   flex-direction: row;
   justify-content: center;
   align-items: center;
-
+  
   border-radius: 8px;
-  width: 77%;
+  width: 100%;
   background-color: ${({theme}) => theme.colors.card};
+  height: 42px;
 `;
 
 export const CardDay = styled.TouchableOpacity<CardDayProps>`
@@ -17,25 +18,25 @@ export const CardDay = styled.TouchableOpacity<CardDayProps>`
   align-items: center;
   background-color: ${({theme}) => theme.colors.card};
   padding: 9px;
-  width: 89px;
-  height: 34px;
+  width: 33%;
+  height: 100%;
 
 
   ${({isActive, theme}) => isActive && css`
-    background: #FFFFFF;
+    background: ${({theme}) => theme.colors.primary};
     border: 1px solid ${lighten(0.2, theme.colors.primary)};
     border-radius: 4px;
   `}
 `;
 
 export const CardDayText = styled.Text<CardDayProps>`
-  font-size: ${({theme}) => theme.sizes.small}px;
+  font-size: ${({theme}) => theme.sizes.default}px;
   font-family: ${({theme}) => theme.fonts.regular};
   color: ${({theme}) => theme.colors.description};
 
 
   ${props => props.isActive && css`
-    color: ${({theme}) => theme.colors.primary};
+    color: #FFF;
     font-family: ${({theme}) => theme.fonts.bold};
   `}
 `;
